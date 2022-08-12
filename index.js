02ec1b60bfad709256cbdb21695fac78d589f529
Olha Mostipan <o.t.panchenko@gmail.com>
2022-08-12 13:24:29 +0300

    add weather emoji

--- index.js
+++ index.js
@@ -2,30 +2,34 @@ let apiKey = "361cdf95371294414c2f6523bf4e6b9b";
 function showTemperature(response) {
   let temperature = Math.round(response.data.main.temp);
   let temperatureContainer = document.querySelector(".temperature");
-  temperatureContainer.innerHTML = temperature;
 
   let feelingTemperature = Math.round(response.data.main.feels_like);
   let feelingTemperatureContainer = document.querySelector(
     ".temperature-feeling"
   );
-  feelingTemperatureContainer.innerHTML = `Feels like ${feelingTemperature}°C`;
-
+  
   let humidity = response.data.main.humidity;
   let humidityContainer = document.querySelector(".humidity");
-  humidityContainer.innerHTML = `Humidity: ${humidity}%`;
-
+  
   let wind = response.data.wind.speed;
   let windContainer = document.querySelector(".wind");
-  windContainer.innerHTML = `Wind: ${wind}km/h`;
-
+  
   let weatherDescription = response.data.weather[0].main;
   let weatherDescriptionContainer =
-    document.querySelector(".emoji-description");
-  weatherDescriptionContainer.innerHTML = weatherDescription;
-
+  document.querySelector(".emoji-description");
+  
   let city = response.data.name;
   let cityContainer = document.querySelector(".city");
+
+  let weatherEmoji = document.querySelector(".main-emoji")
+  
+  temperatureContainer.innerHTML = temperature;
+  feelingTemperatureContainer.innerHTML = `Feels like ${feelingTemperature}°C`;
+  humidityContainer.innerHTML = `Humidity: ${humidity}%`;
+  windContainer.innerHTML = `Wind: ${wind}km/h`;
+  weatherDescriptionContainer.innerHTML = weatherDescription;
   cityContainer.innerHTML = city;
+  weatherEmoji.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 }
 function search(event) {
   event.preventDefault();
@@ -39,18 +43,6 @@ function search(event) {
 }
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", search);
-
-function showPosition(position) {
-  let lon = Math.round(position.coords.longitude);
-  let lat = Math.round(position.coords.latitude);
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showTemperature);
-}
-function getCurrentPosition() {
-  navigator.geolocation.getCurrentPosition(showPosition);
-}
-let currentButton = document.querySelector(".location-button");
-currentButton.addEventListener("click", getCurrentPosition);
 
 let currentDate = new Date();
 let time = document.querySelector(".time");
